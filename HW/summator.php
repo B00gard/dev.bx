a09915f06eeb63f4b0f2204b4591d58883b24c1c
DESKTOP-4DE3CKT\lina_ <angelinka121212@mail.ru>
2020-11-05 13:05:57 +0200

    Суммирование произвольного количества чисел, введенных пользователем из консоли

---
+++ HW/summator.php
@@ -0,0 +1,24 @@
+<?php
+
+// считать произвольное количество чисел от пользователя
+// сложить их, вывести результат
+
+require_once("readFromConsole.php");
+
+function Sum($sum): int
+{
+	$input = true;
+
+	while ($input !== null)
+	{
+		$input = readFromConsole('Введите число');
+		if (is_numeric($input))
+		{
+			$sum += $input;
+		}
+	}
+	return $sum;
+}
+
+$sum = 0;
+echo 'Результат сложения чисел: '.Sum($sum);
