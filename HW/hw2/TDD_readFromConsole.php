d62ceed99c4f2f0c366b1776e8540cdc71023f60
DESKTOP-4DE3CKT\lina_ <angelinka121212@mail.ru>
2020-11-09 16:38:19 +0200

    Функция readFromConsole, написанная при помощи подхода TDD

---
+++ HW/hw2/TDD_readFromConsole.php
@@ -0,0 +1,28 @@
+<?php
+
+function readFromConsole(string $question, string $test_input = null)
+{
+	echo $question.': ';
+	$input = $test_input=== null ? trim(fgets(STDIN)): $test_input;
+
+	switch ($input)
+	{
+		case '':
+			return '';
+
+		case '!stop':
+			return null;
+
+		case 'true':
+			return true;
+
+		case 'false':
+			return false;
+
+		case is_numeric($input):
+			return +$input;
+
+		default:
+			return (string)$input;
+	}
+}
