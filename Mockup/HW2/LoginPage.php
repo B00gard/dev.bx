6c496fd0e0a497b4d248696f4d3cf7cbf4a21a75
DESKTOP-4DE3CKT\lina_ <angelinka121212@mail.ru>
2020-12-04 16:30:18 +0200

    Страница

---
+++ Mockup/HW2/LoginPage.php
@@ -0,0 +1,31 @@
+<?php include_once "header.php";?>
+
+
+<div class="container LoginPage">
+	<div class="content-form">
+		<h4 class="text-center">Try the product out for free.</h4>
+		<form>
+			<div class="form-group">
+				<label for="exampleFormControlInput1">
+					<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="email"/>
+				</label>
+			</div>
+		</form>
+		<form>
+			<div class="form-group">
+				<label for="exampleFormControlInput1">
+					<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="password"/>
+				</label>
+			</div>
+		</form>
+		<div class="col text-center">
+			<a href="" class="btn btn-lg btn-dark">Start free trial</a>
+		</div>
+
+	</div>
+
+</div>
+
+
+
+<?php include_once "footer2.php";
