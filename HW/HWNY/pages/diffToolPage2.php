4c470123695ae61e331dfd15d3427bd9874183f9
DESKTOP-4DE3CKT\lina_ <angelinka121212@mail.ru>
2021-01-27 20:12:08 +0200

    Функции, которые создают страницу и путь к ней вынесены в отдельный файл. Добавлено подключение данного файла.

--- HW/HWNY/pages/diffToolPage2.php
+++ HW/HWNY/pages/diffToolPage2.php
@@ -3,63 +3,12 @@
 require_once("../lib/diffToolClass.php");
 require_once("../lib/pageClass.php");
 require_once("../lib/db_config.php");
+require_once ("../lib/createPageOutput.php");
 
 $text1 = $_POST['text1'] ?? '';
 $text2 = $_POST['text2'] ?? '';
 
 $t = DiffTool::compareTexts($text1, $text2);
-
-function createPage(array $texts, string $pageName)
-{
-	$f = fopen($pageName, 'w');
-
-	fwrite($f, Page::header());
-	echo Page::header();
-
-	fwrite($f, Page::beforeText());
-	echo Page::beforeText();
-
-	$text1 = DiffTool::colorText($texts['text1']);
-	echo $text1;
-
-	echo Page::betweenTexts();
-
-	$text2 = DiffTool::colorText($texts['text2']);
-	echo $text2;
-
-	echo Page::afterText();
-
-	fwrite($f, "			$text1");
-	fwrite($f, Page::betweenTexts());
-	fwrite($f, "			$text2");
-	fwrite($f, Page::afterText());
-
-	fclose($f);
-}
-
-function createPathPage($texts): array
-{
-	try
-	{
-		$rand = (string)random_int(1, 100);
-	}
-	catch (Exception $e)
-	{
-		$rand = rand(1, 100);
-	}
-
-	$pageName = "DBpages/$rand.html";
-	$path = "../".$pageName;
-
-	createPage($texts, $path);
-
-	$sha = sha1_file($path);
-	$newPageName = "DBpages/$sha$rand.html";
-	$newPath = "../".$newPageName;
-
-	rename($path, $newPath);
-	return ['pageName'=>$newPageName, 'sha'=>$sha];
-}
 
 $pageName = 'DBpages/1.html';
 $sha = 'none';
