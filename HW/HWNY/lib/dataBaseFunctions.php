aa1f0ba3f76420799567cadc7fdbbc203d626640
DESKTOP-4DE3CKT\lina_ <angelinka121212@mail.ru>
2021-01-27 20:24:51 +0200

    Функции для работы с БД

---
+++ HW/HWNY/lib/dataBaseFunctions.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("../lib/db_config.php");
+
+function insert(string $pageName, string $sha)
+{
+	$mysqli = new mysqli(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+	$date = date('Y-n-j');
+	$time = date("H:i:s");
+	$query = "INSERT INTO diff_tool (LINK, DATE, TIME, SHA_1_FILE) VALUES ('{$pageName}', '{$date}', '{$time}', '{$sha}')";
+	$mysqli->query($query);
+	$mysqli->close();
+}
+
