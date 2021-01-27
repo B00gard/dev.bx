31e5117564e323dc2d1e82dbb3284010d261dfbe
DESKTOP-4DE3CKT\lina_ <angelinka121212@mail.ru>
2021-01-27 20:24:26 +0200

    Всё, что относилось к работе с БД (функция и подключение конфигурации БД) вынесено в отдельный файл. Добавлено подключение этого файла

--- HW/HWNY/pages/diffToolPage2.php
+++ HW/HWNY/pages/diffToolPage2.php
@@ -1,8 +1,8 @@
 <?php
 
 require_once("../lib/diffToolClass.php");
-require_once("../lib/db_config.php");
 require_once("../lib/createPageOutput.php");
+require_once("../lib/dataBaseFunctions.php");
 
 $text1 = $_POST['text1'] ?? '';
 $text2 = $_POST['text2'] ?? '';
@@ -12,15 +12,5 @@ $t = DiffTool::compareTexts($text1, $text2);
 $pageName = 'DBpages/1.html';
 $sha = 'none';
 extract(createPathPage($t));
-
-function insert(string $pageName, string $sha)
-{
-	$mysqli = new mysqli(HOSTNAME, USERNAME, PASSWORD, DATABASE);
-	$date = date('Y-n-j');
-	$time = date("H:i:s");
-	$query = "INSERT INTO diff_tool (LINK, DATE, TIME, SHA_1_FILE) VALUES ('{$pageName}', '{$date}', '{$time}', '{$sha}')";
-	$mysqli->query($query);
-	$mysqli->close();
-}
 
 insert($pageName, $sha);
